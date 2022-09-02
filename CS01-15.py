fb6cee9bbbf9170e21a7af49c69c92ce21c46ebb
UpDowLR <105915863+UpDowLR@users.noreply.github.com>
2022-09-02 16:20:27 +0700

    15

--- CS01-15.py
+++ CS01-15.py
@@ -1,5 +1,12 @@
-num = int(input('enter your loop'))
-total = []
-for i in rang(num):
-    data = int (input('enter your data:'))
-    total += [data]
+n = int(input('Enter Your Loop'))
+Total = []
+print('Enter Your Data:')
+for i in range(n):
+    data = int(input(''))
+    Total += [data]
+print(Total,end='')
+Total.sort()
+print("----->",end='')
+print(Total)
+print("Min -> ",Total[0])
+print("Max -> ",Total[-1])
